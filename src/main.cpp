313bb9cafab54490824c8bbe54d331c9f2285a54
Nuur-R <firdausnuurr@gmail.com>
2023-01-30 17:56:57 +0700

    Sensor dan Display OK

--- src/main.cpp
+++ src/main.cpp
@@ -4,139 +4,146 @@
 #include <DMD2.h>
 #include <fonts/SystemFont5x7.h>
 #include <fonts/Arial_Black_16.h>
-
-#include <Wire.h>
-#include <SPI.h>
+#include "DHT.h"
 #include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
 
-// Pin definitions
-#define windPin 13 // Receive the data from sensor
+#define mq2pin D2
+#define DHTPIN D1
+#define windSpeedPin A0
+#define DHTTYPE DHT22
 
-// Constants definitions
-const float pi = 3.14159265; // pi number
-int period = 10000;          // Measurement period (miliseconds)
-int delaytime = 10000;       // Time between samples (miliseconds)
-int radio = 90;              // Distance from center windmill to outer cup (mm)
-int jml_celah = 18;          // jumlah celah sensor
+DHT dht(DHTPIN, DHTTYPE);
 
-// Variable definitions
-unsigned int Sample = 0;  // Sample number
-unsigned int counter = 0; // B/W counter for sensor
-unsigned int RPM = 0;     // Revolutions per minute
-float speedwind = 0;      // Wind speed (m/s)
+int mq2Value = 0;
+int kelembaban = 0;
+int suhu = 0;
+int indeksPanas = 0;
+int windSpeed = 0;
+int windSpeedMph = 0;
+int altitude = 702;
+int pressure = 0;
+String gasStatus = "No Gas Detected";
 
+const int WIDTH = 4;
+const uint8_t *FONT = Arial_Black_16;
 
-void addcount()
+SPIDMD  dmd(WIDTH,1);
+DMD_TextBox box(dmd);
+class Task
 {
-    counter++;
-}
-// Measure wind speed
-void windvelocity()
-{
-    pinMode(windPin, INPUT_PULLUP);
-    speedwind = 0;
-    counter = 0;
-    attachInterrupt(windPin, addcount, CHANGE);
-    unsigned long millis();
-    long startTime = millis();
-    while (millis() < startTime + period)
+public:
+    Task(unsigned long interval, void (*callback)())
     {
+        this->interval = interval;
+        this->callback = callback;
+        this->nextRun = millis() + interval;
     }
 
-    detachInterrupt(1);
-}
+    void update()
+    {
+        if (millis() >= nextRun)
+        {
+            nextRun = millis() + interval;
+            callback();
+        }
+    }
 
-void RPMcalc()
+private:
+    unsigned long interval;
+    unsigned long nextRun;
+    void (*callback)();
+};
+
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
 {
-    RPM = ((counter / jml_celah) * 60) / (period / 1000); // Calculate revolutions per minute (RPM)
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-void WindSpeed()
-{
-    speedwind = ((2 * pi * radio * RPM) / 60) / 1000; // Calculate wind speed on m/s
-}
+// = = = = = = = = === = = =  =
 
+Task dataUpdate(1000, []()
+                {
+                    float sensorValue = analogRead(A0);
+                    float voltage = (sensorValue / 1023) * 5;
 
+                    windSpeed = mapfloat(voltage, 0.4, 2, 0, 32.4);
+                    windSpeedMph = ((windSpeed * 3600) / 1609.344);
+
+                    if (windSpeed <= 0)
+                    {
+                        windSpeed = windSpeed * -1;
+                    }
+                    if (windSpeedMph <= 0)
+                    {
+                        windSpeedMph = windSpeedMph * -1;
+                    }
+
+                    mq2Value = digitalRead(mq2pin);
+                    kelembaban = dht.readHumidity();
+                    suhu = dht.readTemperature();
+                    indeksPanas = dht.computeHeatIndex(suhu, kelembaban, false);
+                    pressure = 0;
+
+                    if (isnan(kelembaban) || isnan(suhu)) {
+                        Serial.println(F("Failed to read from DHT sensor!"));
+                        return;
+                    }
+                    if (mq2Value > 0)
+                    {
+                        gasStatus = "Udara Kotor";
+                    }
+                    else
+                    {
+                        gasStatus = "Udara Bersih";
+                    }
+
+                    Serial.println("Data Updated");
+                    Serial.println("mq2Value    : " + String(mq2Value)+" ppm");
+                    Serial.println("gasStatus   : " + gasStatus);
+                    Serial.println("suhu        : " + String(suhu)+" C");
+                    Serial.println("kelembaban  : " + String(kelembaban)+" %");
+                    Serial.println("indeksPanas : " + String(indeksPanas)+" C");
+                    Serial.println("windSpeed   : " + String(windSpeed)+" m/s");
+                    Serial.println();
+                });
+Task DisplayP10(1000, []()
+                {
+                    String MESSAGE = "Selamat Datang di PIKSI GANESHA Bandung || Mini Weather Station Ganesha Purple || Kualitas Udaraa : " + gasStatus + " || Temperatur : " + String(suhu) + " C || Kelembaban : " + String(kelembaban) + " % || Indeks Panas : " + String(indeksPanas) + " C || Kecepatan Angin : " + String(windSpeed) + " m/s      ";
+                    Serial.println(MESSAGE);
+                    const char *next = MESSAGE.c_str();
+                    while(*next) {
+                        Serial.print(*next);
+                        box.print(*next);
+                        delay(200);
+                        next++;
+                    }
+                });
 
 void setup()
 {
-    // Set the pins
-    pinMode(D4, INPUT);
-    digitalWrite(2, HIGH);
+    pinMode(mq2pin, INPUT);
+    pinMode(DHTPIN, INPUT);
+    pinMode(windSpeedPin, INPUT);
 
-    // sets the serial port to 9600
-    Serial.begin(9600);
+    Serial.begin(115200);
+    Serial.println("Wind Speed Sensor");
+    delay(3000);
+    dht.begin();
 
-    // Splash screen
-    Serial.println("ANEMOMETER");
-    Serial.println("****");
-    Serial.println("Based on depoinovasi anemometer sensor");
-    Serial.print("Sampling period: ");
-    Serial.print(period / 1000);
-    Serial.print(" seconds every ");
-    Serial.print(delaytime / 1000);
-    Serial.println(" seconds.");
-    Serial.println("* You could modify those values on code *");
-    Serial.println();
+    dmd.setBrightness(255);
+    dmd.selectFont(FONT);
+    dmd.begin();
 }
 
 void loop()
 {
-    Sample++;
-    Serial.print(Sample);
-    Serial.print(": Start measurement…");
-    windvelocity();
-    Serial.println(" finished.");
-    Serial.print("Counter: ");
-    Serial.print(counter);
-    Serial.print("; RPM: ");
-    RPMcalc();
-    Serial.print(RPM);
-    Serial.print("; Wind speed: ");
-    WindSpeed();
-    Serial.print(speedwind);
-    Serial.print(" [m/s]");
-    Serial.println();
-    delay(2000);
+    dataUpdate.update();
+    DisplayP10.update();
 }
 
 
 
-// void setup() {
-//     Serial.begin(115200);
-//     Serial.println("Hello World!");
 
-//     pinMode(A0, INPUT);
-// }
-
-// void loop() {
-//     int mq2Value = analogRead(A0);
-//     Serial.print("Smoke : ");
-//     Serial.println(mq2Value);
-//     delay(1000);
-// }
-
-// // Set Width to the number of displays wide you have
-// const int WIDTH = 4;
-
-// // You can change to a smaller font (two lines) by commenting this line,
-// // and uncommenting the line after it:
-// const uint8_t *FONT = Arial_Black_16;
-// //const uint8_t *FONT = SystemFont5x7;
-
-// const char *MESSAGE = "Hello Wolrd! - ";
-
-// SPIDMD  dmd(WIDTH,1);  // DMD controls the entire display
-// DMD_TextBox box(dmd);  // "box" provides a text box to automatically write to/scroll the display
-
-// // the setup routine runs once when you press reset:
-// void setup() {
-//   Serial.begin(9600);
-//   dmd.setBrightness(255);
-//   dmd.selectFont(FONT);
-//   dmd.begin();
-// }
 
 // // the loop routine runs over and over again forever:
 // void loop() {
