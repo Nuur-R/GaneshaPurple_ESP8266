17ad0c13548ddfbccec23eb368a97d247ae851a4
Nuur-R <firdausnuurr@gmail.com>
2023-01-30 14:08:41 +0700

    MQ2 Analog Read OK

--- src/main.cpp
+++ src/main.cpp
@@ -5,35 +5,61 @@
 #include <fonts/SystemFont5x7.h>
 #include <fonts/Arial_Black_16.h>
 
-// Set Width to the number of displays wide you have
-const int WIDTH = 4;
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
 
-// You can change to a smaller font (two lines) by commenting this line,
-// and uncommenting the line after it:
-const uint8_t *FONT = Arial_Black_16;
-//const uint8_t *FONT = SystemFont5x7;
 
-const char *MESSAGE = "Hello Wolrd! - ";
 
-SPIDMD  dmd(WIDTH,1);  // DMD controls the entire display
-DMD_TextBox box(dmd);  // "box" provides a text box to automatically write to/scroll the display
 
-// the setup routine runs once when you press reset:
 void setup() {
-  Serial.begin(9600);
-  dmd.setBrightness(255);
-  dmd.selectFont(FONT);
-  dmd.begin();
+    Serial.begin(115200);
+    Serial.println("Hello World!");
+
+    pinMode(A0, INPUT);
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
-  const char *next = MESSAGE;
-  while(*next) {
-    Serial.print(*next);
-    box.print(*next);
-    delay(200);
-    next++;
-  }
+void loop() { 
+    int mq2Value = analogRead(A0);
+    Serial.print("Smoke : ");
+    Serial.println(mq2Value);
+    delay(1000);
 }
+
+
+
+
+
+// // Set Width to the number of displays wide you have
+// const int WIDTH = 4;
+
+// // You can change to a smaller font (two lines) by commenting this line,
+// // and uncommenting the line after it:
+// const uint8_t *FONT = Arial_Black_16;
+// //const uint8_t *FONT = SystemFont5x7;
+
+// const char *MESSAGE = "Hello Wolrd! - ";
+
+// SPIDMD  dmd(WIDTH,1);  // DMD controls the entire display
+// DMD_TextBox box(dmd);  // "box" provides a text box to automatically write to/scroll the display
+
+// // the setup routine runs once when you press reset:
+// void setup() {
+//   Serial.begin(9600);
+//   dmd.setBrightness(255);
+//   dmd.selectFont(FONT);
+//   dmd.begin();
+// }
+
+// // the loop routine runs over and over again forever:
+// void loop() {
+//   const char *next = MESSAGE;
+//   while(*next) {
+//     Serial.print(*next);
+//     box.print(*next);
+//     delay(200);
+//     next++;
+//   }
+// }
 
